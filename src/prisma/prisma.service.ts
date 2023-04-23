b992f5d53c63d4a44dcae5f310326aa6844cae07
Marcin <wanago.marcin@gmail.com>
2023-04-23 22:50:40 +0200

    feat(Categories): add the middleware for soft deletes

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -8,19 +8,59 @@ export class PrismaService
 {
   async onModuleInit() {
     await this.$connect();
-    this.$use(this.loggingMiddleware);
+    this.$use(this.categorySoftDeleteMiddleware);
+    this.$use(this.categoryFindMiddleware);
   }
 
-  loggingMiddleware: Prisma.Middleware = async (params, next) => {
-    console.log(
-      `${params.action} ${params.model} ${JSON.stringify(params.args)}`,
-    );
+  categoryFindMiddleware: Prisma.Middleware = async (params, next) => {
+    if (params.model !== 'Category') {
+      return next(params);
+    }
+    if (params.action === 'findUnique' || params.action === 'findFirst') {
+      return next({
+        ...params,
+        action: 'findFirst',
+        args: {
+          ...params.args,
+          where: {
+            ...params.args?.where,
+            deletedAt: null,
+          },
+        },
+      });
+    }
+    if (params.action === 'findMany') {
+      return next({
+        ...params,
+        args: {
+          ...params.args,
+          where: {
+            ...params.args?.where,
+            deletedAt: null,
+          },
+        },
+      });
+    }
+    return next(params);
+  };
 
-    const result = await next(params);
-
-    console.log('Result: ', result);
-
-    return result;
+  categorySoftDeleteMiddleware: Prisma.Middleware = async (params, next) => {
+    if (params.model !== 'Category') {
+      return next(params);
+    }
+    if (params.action === 'delete') {
+      return next({
+        ...params,
+        action: 'update',
+        args: {
+          ...params.args,
+          data: {
+            deletedAt: new Date(),
+          },
+        },
+      });
+    }
+    return next(params);
   };
 
   async onModuleDestroy() {
